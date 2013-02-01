930362fa1a433f1a6714681e205e4ab584cfa7e9
Joel dos Santos <joel@dossantos.eng.br>
2013-02-01 11:13:35 -0200

    Adicionando site de CFPs.

--- CFP_files/CFP.js
+++ CFP_files/CFP.js
@@ -1,11 +1,11 @@
 // Created by iWeb 3.0.4 local-build-20130201
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#c0c0c0',opacity:0.750000}),shadow_2:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#c0c0c0',opacity:0.750000}),shadow_3:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#c0c0c0',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#c0c0c0',opacity:0.750000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('CFP_files/CFPMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('CFP_files','shapeimage_2','0');IMpreload('CFP_files','shapeimage_3','0');IMpreload('CFP_files','shapeimage_4','0');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('CFP_files','shapeimage_6','0');IMpreload('CFP_files','shapeimage_10','0');IMpreload('CFP_files','shapeimage_14','0');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
