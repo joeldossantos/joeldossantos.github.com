1c86447f5eb72549b24a087ab7d1fae79e8f7118
Joel dos Santos <joel@dossantos.eng.br>
2013-02-01 09:43:19 -0200

    Removendo conteúdo antigo do site.

--- contents/publications.js
+++
@@ -1,7 +0,0 @@
----
-layout: content
-title: publicacoes
----
-
-<h3>Publica&ccedil;&otilde;es</h3>\
-{% for post in site.categories.publicacoes %}<p><a href="{{ post.url }}"><img src="images/item.png" width="20"/> {{ post.title }}</a></p>{% endfor %}
--- contents/tutorials.js
+++
@@ -1,7 +0,0 @@
----
-layout: content
-title: tutoriais
----
-
-<h3>Tutoriais</h3>\
-{% for post in site.categories.tutoriais %}<p><a href="{{ post.url }}"><img src="images/item.png" width="20"/> {{ post.title }}</a></p>{% endfor %}
