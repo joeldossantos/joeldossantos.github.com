ca3f9592342c0a5d49415ba6af2b2f71d06854b8
Joel dos Santos <joel@dossantos.eng.br>
2013-02-04 23:40:26 -0200

    Atualizando deadline.

--- CFP_files/CFP.js
+++ CFP_files/CFP.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.4 local-build-20130201
+// Created by iWeb 3.0.4 local-build-20130204
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000}),reflection_0:new IWReflection({opacity:0.50,offset:1.00}),shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
