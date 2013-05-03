96369b1ca7b9a6da98f14d2e2d3c715735f37dd9
Joel dos Santos <joel@dossantos.eng.br>
2013-05-03 09:01:00 -0300

    Novos CFP.

--- CFP_files/CFP.js
+++ CFP_files/CFP.js
@@ -1,11 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20130404
+// Created by iWeb 3.0.4 local-build-20130503
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000}),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('CFP_files/CFPMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('CFP_files','shapeimage_6','0');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
