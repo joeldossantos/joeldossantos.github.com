bdb3698e141f557a5695cd11c89003e9c26c5970
Joel dos Santos <joel@dossantos.eng.br>
2013-10-11 12:32:57 -0300

    cfp

--- CFP_files/CFP.js
+++ CFP_files/CFP.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.4 local-build-20131001
+// Created by iWeb 3.0.4 local-build-20131011
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000}),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(3.5355,3.5355),color:'#c0c0c0',opacity:0.500000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
